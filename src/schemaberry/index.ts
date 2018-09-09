a1e56fa8d135a125acfdcf7258afee2b554816a1
tgorka <tomasz.gorka@carmoov.io>
2018-09-09 19:43:50 +0200

    create template for angular 6 app

--- src/schemaberry/index.ts
+++ src/schemaberry/index.ts
@@ -1,10 +1,30 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+/**
+ * @license
+ * Tomasz Gorka <https://tomasz.gorka.org.pl>
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/tgorka/schemaberry/LICENSE
+ */
+import { strings } from "@angular-devkit/core";
+import {
+    Rule,
+    apply,
+    mergeWith,
+    template,
+    url,
+} from "@angular-devkit/schematics";
+import { Schema as Options } from "./schema";
 
 
-// You don't have to export the function as default. You can also have more than one rule factory
+// You don"t have to export the function as default. You can also have more than one rule factory
 // per file.
-export function schemaberry(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
-  };
+export function schemaberry(options: Options): Rule {
+    return mergeWith(apply(url("./files"), [
+        template({
+            utils: strings,
+            ...options,
+            "dot": ".",
+            //latestVersions,
+        }),
+    ]));
 }
